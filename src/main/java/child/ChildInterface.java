6bb88fefede5eeb07d05eed6452705d1b6314dc3
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-01-30 20:52:08 +0100

    examples

---
+++ src/main/java/child/ChildInterface.java
@@ -0,0 +1,7 @@
+package child;
+
+/**
+ * Created by mtumilowicz on 2019-01-30.
+ */
+public interface ChildInterface {
+}
---
+++ src/main/java/parent/ParentInterface.java
@@ -0,0 +1,7 @@
+package parent;
+
+/**
+ * Created by mtumilowicz on 2019-01-30.
+ */
+public interface ParentInterface {
+}
