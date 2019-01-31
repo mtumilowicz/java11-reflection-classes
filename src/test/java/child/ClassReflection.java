465773430ae8172b8d480b0018ece7cb99e8e845
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-01-31 19:39:44 +0100

    tests partitioning

--- src/test/java/child/ClassReflection.java
+++ src/test/java/child/ClassReflection.java
@@ -14,28 +14,51 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class ClassReflection {
 
-//    private Class<Child> childClass = Child.class;
-
+    private Class<Child> childClass = Child.class;
+    
     @Test
-    void info() {
-        Class<Child> childClass = Child.class;
-
+    void modifiers() {
         assertEquals(Modifier.toString(childClass.getModifiers() & Modifier.classModifiers()), "public abstract");
-
+    }
+    
+    @Test
+    void name() {
         assertEquals(childClass.getName(), "child.Child");
-
+    }
+    
+    @Test
+    void simpleName() {
         assertEquals(childClass.getSimpleName(), "Child");
-
+    }
+    
+    @Test
+    void superclass() {
         assertEquals(childClass.getSuperclass(), Parent.class); // if no superclass other than object => null
-
+    }
+    
+    @Test
+    void interfaces() {
         assertArrayEquals(childClass.getInterfaces(), new Class<?>[]{ChildInterface.class, Serializable.class});
-
+    }
+    
+    @Test
+    void typeParameters() {
         assertEquals(Arrays.toString(childClass.getTypeParameters()), "[T]");
-
+    }
+    
+    @Test
+    void genericString() {
         assertEquals(childClass.toGenericString(), "public abstract class child.Child<T>");
-
+    }
+    
+    @Test
+    void packageName() {
         assertEquals(childClass.getPackageName(), "child");
-
-        assertTrue(childClass.getPackage().getName().contains("unnamed module"));
+    }
+    
+    @Test
+    void moduleName() {
+        assertTrue(childClass.getModule().toString().contains("unnamed module"));
+        assertNull(childClass.getModule().getName());
     }
 }
